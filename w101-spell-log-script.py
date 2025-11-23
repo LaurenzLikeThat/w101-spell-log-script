77cc50f2754e7a2dc7b460d5cb99a59632efb098
ItzGray <63925441+ItzGray@users.noreply.github.com>
2025-11-23 16:17:51 -0600

    Add script

---
+++ w101-spell-log-script.py
@@ -0,0 +1,29 @@
+import psutil
+
+def main():
+    for pid in psutil.pids():
+        try:
+            if psutil.Process(pid).name() == "WizardGraphicalClient.exe":
+                path = psutil.Process(pid).exe()
+        except psutil.NoSuchProcess:
+            continue
+    print(f"Log path: {path}")
+    log_path = f"{path.split("\\WizardGraphicalClient.exe")[0]}\\WizardClient.log"
+    old_length = 0
+    while True:
+        with open(log_path, "r") as log:
+            log.seek(0)
+            log_lines = log.readlines()
+            new_length = len(log_lines)
+            new_log_lines = []
+            if new_length > old_length:
+                new_log_lines = log_lines[old_length:new_length]
+            elif new_length < old_length:
+                new_log_lines = log_lines
+            for line in new_log_lines:
+                if "[STAT] CombatDetails   CombatResolver::ResolveCombatRound." in line:
+                    print(line[77:])
+            old_length = new_length
+
+if __name__ == "__main__":
+    main()
