b871d75ee7997be0048d78bf550451b7f2c30db3
LaurenzLikeThat <misslaurenz@proton.me>
2025-12-23 18:47:44 -0500

    optimize script to not require excessive I/O operations

--- w101-spell-log-script.py
+++ w101-spell-log-script.py
@@ -9,21 +9,16 @@ def main():
             continue
     log_path = "{0}\\WizardClient.log".format(path.split("\\WizardGraphicalClient.exe")[0])
     print(f"Log path: {log_path}")
-    old_length = 0
-    while True:
-        with open(log_path, "r") as log:
-            log.seek(0)
+    log = open(log_path, "r")
+    log.seek(0, 2)
+    try:
+        while True:
             log_lines = log.readlines()
-            new_length = len(log_lines)
-            new_log_lines = []
-            if new_length > old_length:
-                new_log_lines = log_lines[old_length:new_length]
-            elif new_length < old_length:
-                new_log_lines = log_lines
-            for line in new_log_lines:
+            for line in log_lines:
                 if "[STAT] CombatDetails   CombatResolver::ResolveCombatRound." in line:
                     print(line[77:])
-            old_length = new_length
+    finally:
+        log.close()
 
 if __name__ == "__main__":
     main()
